b92731b4081061dbcb205315e00564af48774dec
irwanto02 <107055600+irwanto02@users.noreply.github.com>
2024-01-01 09:14:58 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -130,7 +130,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
       if (distance < 0) {
         clearInterval(countdown);
-        document.getElementById("countdown").innerHTML = "I Love You !";
+        document.getElementById("countdown").innerHTML = "I Love You ! &#x2764;";
         showFireworks();
         playTrumpetSound();
       }
